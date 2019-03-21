ba99f8be8fdc51ac6fc82d64726b09a09c80562b
Zahra Khoshmanesh <zkh@iastate.edu>
2019-03-20 20:57:02 -0500

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,12 @@
+#' print hello "name".
+#'
+#' @param x A number.
+#' @export
+#' @return print name.
+#' @examples
+#' hello("Sam)
+
+
+hello<- function(s){
+  sprintf("hello %s",s)
+  }
---
+++ R/sum3.R
@@ -0,0 +1,14 @@
+#' Add together two numbers.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @param z A number.
+#' @export
+#' @return The sum of \code{x} and \code{y} and \code{z}.
+#' @examples
+#' add(1, 1,2)
+#' add(10, 1,3)
+sum3 <- function(x, y,z) {
+  x + y+z
+}
+
